1fc23d374ee6bb47b72544bf54a9aeef4d4705d0
maazil3861 <maazshaikh7020@gmail.com>
2020-07-05 11:18:17 +0530

    variable syntax

---
+++ python_programs/python_syntax/variables.py
@@ -0,0 +1,7 @@
+# Python has no command for declaring a variable.
+# In Python, variables are created when you assign a value to it.
+
+number = 5
+string_name = "Maaz"
+print(number)
+print(string_name)
