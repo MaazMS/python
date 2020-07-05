7d58b1be88d1225d4b3752779d249dc6a731b970
maazil3861 <maazshaikh7020@gmail.com>
2020-07-05 11:16:09 +0530

    indentation concept

---
+++ python_programs/python_syntax/indentation.py
@@ -0,0 +1,6 @@
+# indention refer to space at the beginning of a code line.
+# python use indention to indicate block of code.
+# The number of spaces is up to you as a programmer, but it has to be at least one.
+
+if 5 > 2:
+    print("five greater then two")
