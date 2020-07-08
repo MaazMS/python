7e6530241a157b6f53f872a63a536f7b27552b93
maazil3861 <maazshaikh7020@gmail.com>
2020-07-08 08:31:28 +0530

    Program of data type

---
+++ python_programs/Data_types/python_data_type.py
@@ -0,0 +1,36 @@
+
+
+name = "Maaz Shaikh"  # Data type is string
+print(type(name))
+
+
+number = 60          # Data type is int
+print(type(number))
+
+non_integer_number = 20.00  # Data type is float
+print(type(non_integer_number))
+
+
+complex_number = 3j    # Data type is complex
+print(type(complex_number))
+
+count = [1, 2, 3]  # Data type is list
+print(type(count))
+
+
+fruites = ("Apple", "Banana", "Manago")  # Data type is tuple
+print(type(fruites))
+
+number_sequence = range(5)   # Data Type is range
+print(number_sequence)
+print(type(number_sequence))
+
+for i in number_sequence:    # example of range
+    print(i)
+
+person = {"name" : "Maaz", "gender" : "Male"}  # Data type is dictionary
+print(type(person))
+
+city = {"pune", "Mumbai"}  # data type is set
+print(type(city))
+
