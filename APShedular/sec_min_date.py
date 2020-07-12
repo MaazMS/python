0ba55c759beff406d53b8e622ff4fa08b0fd21ca
maazil3861 <maazshaikh7020@gmail.com>
2020-07-12 11:02:42 +0530

    The schedule program for sec,min,and every day fix time

---
+++ APShedular/sec_min_date.py
@@ -0,0 +1,24 @@
+import schedule
+import time
+
+def job_by_sec():
+    print("This job work on the every 5 second")
+
+
+def job_by_min():
+    print("This job work on every 1 min ")
+
+
+def job_by_date():
+    print("This job work on every day 11:00")
+
+
+# call the task/job
+schedule.every(5).seconds.do(job_by_sec)
+schedule.every(1).minutes.do(job_by_min)
+schedule.every().day.at("11:00").do(job_by_date)
+
+while True:
+        schedule.run_pending()
+        time.sleep(1)
+
