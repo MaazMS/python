f00b80a2d6d71af0c8bbc0b87608d953c53e4a68
maazil3861 <maazshaikh7020@gmail.com>
2020-07-20 15:06:42 +0530

    insert data using declarative mapping

---
+++ sqlalchemydbEX/CRUD/insert_into_customer.py
@@ -0,0 +1,11 @@
+from sqlalchemydbEX.CRUD.declarative_map import Customer , Engine
+from  sqlalchemy.orm import sessionmaker
+
+Session = sessionmaker(bind= Engine)
+session = Session()
+
+cust_name = input('Enter customer name')
+cust1 = Customer(name = cust_name)
+session.add(cust1)
+session.commit()
+print('insert successful')
