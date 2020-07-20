0850c7e5d7624c0616e030028fb06360b5f1e34a
maazil3861 <maazshaikh7020@gmail.com>
2020-07-20 15:05:51 +0530

    declarative example

---
+++ sqlalchemydbEX/CRUD/declarative_map.py
@@ -0,0 +1,19 @@
+from sqlalchemy import create_engine
+from sqlalchemy import  Column, Integer, String, MetaData
+from sqlalchemy.ext.declarative import  declarative_base
+
+Engine = create_engine('sqlite:///declarative_map.sqlite')
+BASE = declarative_base()  # This base class have configuration
+
+# create Table
+# inherit form base class
+class Customer(BASE):
+    __tablename__ ='Customer'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(200))
+
+    def __str__(self): # object representation
+        return  self.name
+
+# important
+BASE.metadata.create_all(Engine)
