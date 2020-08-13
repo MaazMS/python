4b2ccdbfdd9efa08512fc7d3a4187687b6c50a4d
maazil3861 <maazshaikh7020@gmail.com>
2020-08-13 08:53:44 +0530

    int, float, complex numbers and check it instance class

---
+++ python_programs/number/type_and_instance_of_variable.py
@@ -0,0 +1,19 @@
+
+int_number = 10
+print("int_number is ", int_number)
+print("type of int_number is ",type(int_number))
+print("int_number belong to class int check ", isinstance(int_number, int))
+print()
+print()
+
+float_number = 10.00
+print("float_number is ", float_number)
+print("type of float_number is ",type(float_number))
+print("float_number belong to class float check ", isinstance(float_number, float))
+print()
+print()
+
+complex_number = 10 + 2j
+print("complex_number is ", complex_number)
+print("type of complex_number is ",type(complex_number))
+print("complex_number belong to class complex check ", isinstance(complex_number, complex))
