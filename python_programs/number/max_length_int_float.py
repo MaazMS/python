30e05057d1f3607e15f38c5da1d1dd082efc8775
maazil3861 <maazshaikh7020@gmail.com>
2020-08-19 09:26:25 +0530

    max length of int and  flaot

---
+++ python_programs/number/max_length_int_float.py
@@ -0,0 +1,34 @@
+int_number = 1234567890123456789012345678912345678901234567891234567890
+print("int number ", int_number)
+
+float_number0 = 1.1234567890123450
+print("float number zero   ",float_number0)
+
+float_number1 = 1.1234567890123451
+print("float number one    ",float_number1)
+
+float_number2 = 1.1234567890123452
+print("float number two    ",float_number2)
+
+float_number3 = 1.1234567890123453
+print("float number three  ",float_number3)
+
+float_number4 = 1.1234567890123454
+print("float number four   ",float_number4)
+
+float_number5 = 1.1234567890123455
+print("float number five   ",float_number5)
+
+float_number6 = 1.1234567890123456
+print("float number six    ",float_number6)
+
+float_number7 = 1.1234567890123457
+print("float number seven  ",float_number7)
+
+float_number8 = 1.1234567890123458
+print("float number eight  ",float_number8)
+
+float_number9 = 1.1234567890123459
+print("float number nine   ",float_number9)
+
+
