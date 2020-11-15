c37cfaae9720b4edf24548ba2ae8156b37f9a485
maazil3861 <maazshaikh7020@gmail.com>
2020-11-15 10:19:53 +0530

    url config

--- Django/mywebsite/polls/urls.py
+++ Django/mywebsite/polls/urls.py
@@ -3,6 +3,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # ex: /polls/
     path('', views.index, name = 'index'),
-    path('homepage/', views.homepage, name= 'homepage' )
+    # ex: /polls/homepage/
+    path('homepage/', views.homepage, name= 'homepage' ),
+    # ex: /polls/5/
+    path('<int:question_id>/', views.details, name= 'details'),
+    # ex: /polls/5/results/
+    path('<int:question_id>/results/', views.results, name= 'results'),
+    # ex: /polls/5/vote/
+    path('<int:question_id>/vote/', views.vote, name = 'vote')
 ]
