56d9ed8501e5452cc1be33df68013ad339ce4151
maazil3861 <maazshaikh7020@gmail.com>
2021-01-17 09:06:26 +0530

    even and odd number by for and whileloop

--- python/Data_types/number_type_data/number program/EvenOdd.py
+++ python/Data_types/number_type_data/number program/EvenOdd.py
@@ -23,6 +23,34 @@ class EvenOdd:
                 print("odd number", no)
             else:
                 print("Enter valid odd number")
+    def even_odd_by_for(self):
+
+        no = int(input("Enter number \t"))
+        if no <=0 :
+            print("number is zero or negative")
+
+        for i in range(1, no+1):
+            if i % 2 ==0:
+                print("Even number is \t", i)
+            elif i % 2 !=0:
+                print("Odd number is \t", i)
+    def even_odd_by_while(self):
+
+        no = int(input("Enter number \t"))
+        if (no<=0):
+            print("number is negative or zero")
+        else:
+            i = 1
+            while(i<=no):
+                if i % 2 ==0:
+                    print("Even number is \t", i)
+                elif i % 2 !=0:
+                    print("Odd number is \t", i)
+                i = i + 1
+
+
 obj = EvenOdd()
 obj.even_number()
 obj.odd_number()
+obj.even_odd_by_for()
+obj.even_odd_by_while()
