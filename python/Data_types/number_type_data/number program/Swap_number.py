301d402fcb6c529bf54b8f1f384dad880583b024
maazil3861 <maazshaikh7020@gmail.com>
2021-01-17 09:36:35 +0530

    swap number by third party without third party variable

---
+++ python/Data_types/number_type_data/number program/Swap_number.py
@@ -0,0 +1,27 @@
+class SwapNumber:
+
+    def swap_no_by_third_variable(self):
+        no1 = int(input("Enter number1\t"))
+        no2 = int(input("Enter number2\t"))
+        print("display number1\t",no1)
+        print("display number2\t", no2)
+        temp = no1
+        no1 = no2
+        no2 = temp
+        print("display number1\t", no1)
+        print("display number2\t", no2)
+
+    def swap_no_without_third_variable(self):
+        no1 = int(input("Enter number1\t"))
+        no2 = int(input("Enter number2\t"))
+        print("display number1\t", no1)
+        print("display number2\t", no2)
+        no1 = no1 + no2
+        no2 = no1 - no2
+        no1 = no1 - no2
+        print("display number1\t", no1)
+        print("display number2\t", no2)
+
+obj = SwapNumber()
+obj.swap_no_by_third_variable()
+obj.swap_no_without_third_variable()
