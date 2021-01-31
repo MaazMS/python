bef27a87db7533dd5eb21f75b6bf38deef61dd3d
maazil3861 <maazshaikh7020@gmail.com>
2021-01-31 23:07:32 +0530

    first and last digit of number

---
+++ python/Data_types/number_type_data/number_program/first_last_digit.py
@@ -0,0 +1,26 @@
+class FirstLastDigitNumber:
+
+    def find_first_digit_number(self):
+
+        no = int(input("Enter number \t"))
+
+        if no <= 0:
+            print("Enter positive number")
+        else:
+            while no >= 10:
+                no = no // 10
+            print(no)
+
+    def find_last_digit_number(self):
+
+        no = int(input("Enter number \t"))
+
+        if no <= 0:
+            print("Enter positive number")
+        else:
+            last_digit = no % 10
+            print(last_digit)
+
+obj = FirstLastDigitNumber()
+# obj.find_first_digit_number()
+obj.find_last_digit_number()
