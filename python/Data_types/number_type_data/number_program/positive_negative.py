5d8f215fb5f82efcd5055d457100afa1841452c4
maazil3861 <maazshaikh7020@gmail.com>
2021-01-31 23:11:30 +0530

    positive and negative number

---
+++ python/Data_types/number_type_data/number_program/positive_negative.py
@@ -0,0 +1,21 @@
+class PositiveNegativeNumbe:
+
+    def positive_number(self):
+
+        no = int(input("Enter number \t"))
+        if no > 0 :
+            print("Positive number", no)
+        else:
+            print("It is not Positive number")
+
+    def negative_number(self):
+
+        no = int(input("Enter number \t"))
+        if no < 0:
+            print("negative number", no)
+        else:
+            print("It is not negative number")
+
+obj = PositiveNegativeNumbe()
+obj.positive_number()
+obj.negative_number()
