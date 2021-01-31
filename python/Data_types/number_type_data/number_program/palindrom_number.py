11d5dbb9b12e13f4b6b68eff24ef58465a59c605
maazil3861 <maazshaikh7020@gmail.com>
2021-01-31 23:09:27 +0530

    palindrom of number

---
+++ python/Data_types/number_type_data/number_program/palindrom_number.py
@@ -0,0 +1,23 @@
+class PalindromeNumber:
+
+    def find_palindrom_number(self):
+
+        no = int(input("Enter number \t"))
+        temp_no = no
+        reverse_no = 0
+        if no <= 0:
+            print("Enter positive number ")
+        else:
+            while no > 0:
+                dig = no % 10
+                reverse_no = reverse_no * 10 + dig
+                no = no // 10
+
+            if temp_no == reverse_no:
+                print("The number is palindrome!")
+            else:
+                print("Not a palindrome!")
+
+
+obj = PalindromeNumber()
+obj.find_palindrom_number()
