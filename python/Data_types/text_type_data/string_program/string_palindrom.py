7eeb410aa7252c433c6512f7e7a1deb2c7020513
maazil3861 <maazshaikh7020@gmail.com>
2021-02-04 09:53:15 +0530

    string palindrom using slice or second veriable

---
+++ python/Data_types/text_type_data/string_program/string_palindrom.py
@@ -0,0 +1,26 @@
+class PalindromeString:
+    def palindrome_String(self):
+
+        s1 = input("Enter string")
+        # [::-1] This is slice for reverse string
+        if s1 == s1[::-1]:
+            print("This is a Palindrome String")
+        else:
+            print("This is not a Palindrome String")
+
+    def palindrome_String_by_variable(self):
+
+        s1 = input("Enter string")
+        s2 = ""
+        for i in s1:
+            s2 = i + s2
+
+        if s1 == s2:
+            print("This is a Palindrome String")
+        else:
+            print("This is not a Palindrome String")
+
+
+obj = PalindromeString()
+obj.palindrome_String()
+obj.palindrome_String_by_variable()
