990caa734ed434c797c5fdf59cc4e014d88cc3e9
maazil3861 <maazshaikh7020@gmail.com>
2021-02-04 09:55:39 +0530

    count vowels consonant

---
+++ python/Data_types/text_type_data/string_program/count_vowels_consonant.py
@@ -0,0 +1,47 @@
+class VowelsCOnsonant:
+    def count_Vowels(self):
+
+        str1 = input("Please Enter Your Own String : ")
+        vowels = 0
+
+        for i in str1:
+            if (
+                i == "a"
+                or i == "e"
+                or i == "i"
+                or i == "o"
+                or i == "u"
+                or i == "A"
+                or i == "E"
+                or i == "I"
+                or i == "O"
+                or i == "U"
+            ):
+                vowels = vowels + 1
+        print("Total Number of Vowels in this String = ", vowels)
+
+    def count_Consonants(self):
+
+        str1 = input("Please Enter Your Own String : ")
+        Consonants = 0
+
+        for i in str1:
+            if (
+                i != "a"
+                and i != "e"
+                and i != "i"
+                and i != "o"
+                and i != "u"
+                and i != "A"
+                and i != "E"
+                and i != "I"
+                and i != "O"
+                and i != "U"
+            ):
+                Consonants = Consonants + 1
+        print("Total Number of Consonants in this String = ", Consonants)
+
+
+obj = VowelsCOnsonant()
+obj.count_Vowels()
+obj.count_Consonants()
