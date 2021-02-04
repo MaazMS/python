cfec688cb06f58f84b9d1c6e7ba6e3199b5a9a45
maazil3861 <maazshaikh7020@gmail.com>
2021-02-04 09:49:11 +0530

    concatenate two strings

---
+++ python/Data_types/text_type_data/string_program/concatenate_strings.py
@@ -0,0 +1,11 @@
+class ConcatenateStrings:
+    def concatenate_strings(self):
+
+        string1 = input("Enter first string \t")
+        string2 = input("Enter second string \t")
+
+        print(string1 + string2)
+
+
+obj = ConcatenateStrings()
+obj.concatenate_strings()
