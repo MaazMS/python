20d822e8237fe5c938020679644170336f978e45
maazil3861 <maazshaikh7020@gmail.com>
2021-02-04 09:52:01 +0530

    lower upper string

---
+++ python/Data_types/text_type_data/string_program/lower_upper_string.py
@@ -0,0 +1,14 @@
+class LowerUpperString:
+    def convert_lower_to_upper_string(self):
+
+        lower_case_string = input("Enter lower case string ")
+        print(lower_case_string.upper())
+
+    def convert_upper_to_lower_string(self):
+        upper_case_string = input("Enter upper case string ")
+        print(upper_case_string.lower())
+
+
+obj = LowerUpperString()
+obj.convert_lower_to_upper_string()
+obj.convert_upper_to_lower_string()
