259477707c789ff819611fcd0395896a990612f7
maazil3861 <maazshaikh7020@gmail.com>
2021-02-04 09:53:54 +0530

    copy string

---
+++ python/Data_types/text_type_data/string_program/copy_string.py
@@ -0,0 +1,20 @@
+class CopyString:
+    def copy_one_String_to_another_String(self):
+
+        s1 = input("Enter string \t")
+        s2 = s1
+        print("first way to copy string ", s2)
+
+        # The [:] makes a shallow copy of the array, hence allowing you to modify your copy without damaging the original.
+        s3 = s1[:]
+        print("Second way to copy string ", s2)
+
+        s4 = ""
+
+        for i in s1:
+            s4 = s4 + i
+        print("Third way to copy string ", s2)
+
+
+obj = CopyString()
+obj.copy_one_String_to_another_String()
