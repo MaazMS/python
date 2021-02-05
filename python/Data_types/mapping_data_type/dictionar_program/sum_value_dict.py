3101b528163203b4ff952306905fccb6442ec4c4
maazil3861 <maazshaikh7020@gmail.com>
2021-02-05 10:04:01 +0530

    sum of value in dictionary

---
+++ python/Data_types/mapping_data_type/dictionar_program/sum_value_dict.py
@@ -0,0 +1,8 @@
+class SumDict:
+    def sum_values_dict(self):
+        first_Dict = {"a": 1, "b": 2, "c": 3}
+        print("sum of values in dict", sum(first_Dict.values()))
+
+
+obj = SumDict()
+obj.sum_values_dict()
