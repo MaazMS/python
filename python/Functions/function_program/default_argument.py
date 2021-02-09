fb8e66ed4a94562b0f34de1ae0725547b9f43be2
maazil3861 <maazshaikh7020@gmail.com>
2021-02-09 09:31:16 +0530

    default argument for function parameter

--- python/Functions/function_program/default_argument.py
+++ python/Functions/function_program/default_argument.py
@@ -1,3 +1,15 @@
+"""
+This program for writing for default argument,
+first function call pass value to default argument parameter and non default argument parameter.
+no1 =  2
+no2 =  2
+0.5
+second function call not pass value to default argument parameter and pass value to non default argument parameter.
+no1 =  2
+no2 =  10
+0.1
+"""
+
 def average(no1, no2=10):
     print("no1 = ", no1)
     print("no2 = ", no2)
