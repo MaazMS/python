16037c2364b185cd86cc3d4f7ffab15943c31461
maazil3861 <maazshaikh7020@gmail.com>
2021-02-09 09:40:32 +0530

    keyword argument for function call

--- python/Functions/function_program/keyword_argument.py
+++ python/Functions/function_program/keyword_argument.py
@@ -1,3 +1,9 @@
+"""
+keyword argument is simply say that function definition and function call parameter having same variable name.
+example
+function definition    `def average(no1, no2):`
+function call           average(no2=4 , no1= 2)
+"""
 def average(no1, no2):
     print("no1 = ", no1)
     print("no2 = ", no2)
