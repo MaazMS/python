3e97bbf9aca56b8cb347163ab4d20bb7f4848a9c
maazil3861 <maazshaikh7020@gmail.com>
2021-02-10 08:26:19 +0530

    filter use in lambda

--- python/lambda/lambda_method/filter_lambda.py
+++ python/lambda/lambda_method/filter_lambda.py
@@ -1,4 +1,9 @@
-ls = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-result = list(filter( lambda  no : no % 2 == 0, ls))
-print(result)
+"""
+filter() is use give iterator element.
+Example list element is pass to lambda function and lambda function is under filter method.
+result = list(filter(lambda no: print(no) , ls))
+"""
 
+ls = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+result = list(filter(lambda no: no % 2 == 0, ls))
+print(result)
