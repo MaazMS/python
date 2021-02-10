051b5025110e9e4a90ed26a3a258e7e3d8d22fc6
maazil3861 <maazshaikh7020@gmail.com>
2021-02-10 08:26:54 +0530

    map use in lambda

--- python/lambda/lambda_method/map_lambda.py
+++ python/lambda/lambda_method/map_lambda.py
@@ -1,3 +1,9 @@
+"""
+map() function returns a map object which is an iterator
+Example list element is pass to lambda function and lambda function is under filter method.
+result = list(filter(lambda no: print(no) , ls))
+"""
+
 ls = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-result = list(map( lambda  no : no * 2, ls))
+result = list(map(lambda no: no * 2, ls))
 print(result)
