04de842e3615b99a15161d19cb111fc53e25de25
maazil3861 <maazshaikh7020@gmail.com>
2021-02-10 09:12:49 +0530

    use condition

---
+++ python/List_comprehension/list_comprehension_program/list_condition.py
@@ -0,0 +1,9 @@
+"""
+list comprehension use if condition
+number = [list1 for list1 in list_element if print(list1)]
+print list1 contain list element
+"""
+
+list_element = ['a', 'b',  'c']
+alpha = [list1 for list1 in list_element if 'a' in list1]
+print(alpha)
