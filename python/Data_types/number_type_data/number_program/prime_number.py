8ebf2ce2e5e9edd9098a55903fc07111d075a6a8
MaazMS <maazshaikh7020@gmail.com>
2021-02-28 17:09:02 +0530

    PrimeNumber

--- python/Data_types/number_type_data/number_program/prime_number.py
+++ python/Data_types/number_type_data/number_program/prime_number.py
@@ -1,3 +1,5 @@
+# Prime numbers are numbers that have only 2 factors: 1 and themselves.
+# For example, the first 5 prime numbers are 2, 3, 5, 7, and 11.
 class PrimeNumber:
 
     def find_prime_number(self):
