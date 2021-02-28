15e672b63405b1a594b6b69ef80460e99babab13
MaazMS <maazshaikh7020@gmail.com>
2021-02-28 17:06:59 +0530

    PerfectNumber

--- python/Data_types/number_type_data/number_program/perfect_number.py
+++ python/Data_types/number_type_data/number_program/perfect_number.py
@@ -1,3 +1,5 @@
+# a perfect number is a positive integer that is equal to the sum of its positive divisors, excluding the number itself.
+# For instance, 6 has divisors 1, 2 and 3, and 1 + 2 + 3 = 6, so 6 is a perfect number
 class PerfectNumber:
 
     def find_perfect_number(self):
