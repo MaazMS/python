3e63cdc0517692747cb1568416424c3f5dcfe9ed
MaazMS <maazshaikh7020@gmail.com>
2022-08-03 22:01:50 +0530

    concatenate tuple

---
+++ python/Data_types/sequence_index_type_data/tuple_type_data/tuple_program/concet_tuple.py
@@ -0,0 +1,9 @@
+class ConcetenateTuple: 
+
+    def operation_concatenate():
+        t1 = (1,2,3) 
+        t2 = (4,5,6) 
+        print(t1 + t2) 
+
+obj = ConcetenateTuple() 
+obj.operation_concatenate()
