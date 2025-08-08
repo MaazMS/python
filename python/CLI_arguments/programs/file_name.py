cfab15328f3ebe10597d536255b4f8e354a3e03b
MaazMS <maazshaikh7020@gmail.com>
2025-08-08 17:47:12 +0530

    update CLI argument documentation and program

--- python/CLI_arguments/programs/file_name.py
+++
@@ -1,8 +0,0 @@
-import sys
-ls = sys.argv
-
-print("file name with location \n",ls)
-
-print("length of command line argument", len(ls))
-
-print("file name with location access by command line\n", sys.argv[0])
--- python/CLI_arguments/programs/multiplication.py
+++
@@ -1,4 +0,0 @@
-import sys
-
-ls = sys.argv
-print("product of number", int(ls[1]) * int(ls[2]))
